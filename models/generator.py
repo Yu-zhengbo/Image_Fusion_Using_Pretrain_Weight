216681102b7f26aa249105f9d1f16475fb2ee83a
yuzb2 <yuzb2@knownsec.com>
2023-11-10 13:45:26 +0800

    first commit

---
+++ models/generator.py
@@ -0,0 +1,75 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+
+class Generator(nn.Module):
+    r"""
+    Use to generate fused images.
+    ir + vi -> fus
+    """
+
+    def __init__(self, dim: int = 32, depth: int = 3):
+        super(Generator, self).__init__()
+        self.depth = depth
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, dim, (3, 3), (1, 1), 1),
+            nn.BatchNorm2d(dim),
+            nn.ReLU()
+        )
+
+        self.dense = nn.ModuleList([
+            nn.Sequential(
+                nn.Conv2d(dim * (i + 1), dim, (3, 3), (1, 1), 1),
+                nn.BatchNorm2d(dim),
+                nn.ReLU()
+            ) for i in range(depth)
+        ])
+
+        self.fuse = nn.Sequential(
+            nn.Sequential(
+                nn.Conv2d(dim * (depth + 1), dim * 4, (3, 3), (1, 1), 1),
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.Conv2d(dim * 4, dim * 2, (3, 3), (1, 1), 1),
+                nn.BatchNorm2d(dim * 2),
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.Conv2d(dim * 2, dim, (3, 3), (1, 1), 1),
+                nn.BatchNorm2d(dim),
+                nn.ReLU()
+            ),
+            nn.Sequential(
+                nn.Conv2d(dim, 3, (3, 3), (1, 1), 1),
+                nn.Tanh()
+            ),
+        )
+
+    def forward(self, ir: Tensor): #, vi: Tensor) -> Tensor:
+        # src = torch.cat([ir, vi], dim=1)
+        _,_,h,w = ir.shape
+        x = self.encoder(ir)
+        for i in range(self.depth):
+            t = self.dense[i](x)
+            x = torch.cat([x, t], dim=1)
+        fus = self.fuse(x)
+        if fus.shape[2] != h or fus.shape[3] != w:
+            fus = nn.functional.interpolate(fus, size=(h, w), mode='bilinear')
+        return fus
+
+
+if __name__ == "__main__":
+    # img = torch.randn(2,3,768,1024)
+    # gen_vis = Generator()
+    # gen_inf = Generator()
+    # fus_vis = gen_vis(img)
+    # print(fus_vis.shape)
+    from torchvision.models.vgg import vgg16,vgg13
+    vgg = vgg16(pretrained=False)
+    loss_network = nn.Sequential(*list(vgg.features)[:25]).eval()
+    img = torch.randn(2,3,768,1024)
+    output = loss_network(img)
+    print(output.shape)
---
+++ utils/utils_dataset.py
@@ -0,0 +1,73 @@
+import os
+import torch
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+from torchvision.transforms import Compose, RandomCrop, ToTensor, ToPILImage, CenterCrop, Resize,Grayscale
+from torchvision.transforms import functional as F
+
+
+def resize_image(image, size, letterbox_image=True):
+    iw, ih  = image.size
+    w, h    = size
+    if letterbox_image:
+        scale   = min(w/iw, h/ih)
+        nw      = int(iw*scale)
+        nh      = int(ih*scale)
+
+        image   = image.resize((nw,nh), Image.BICUBIC)
+        new_image = Image.new('RGB', size, (0,0,0))
+        new_image.paste(image, ((w-nw)//2, (h-nh)//2))
+    else:
+        new_image = image.resize((w, h), Image.BICUBIC)
+    return new_image
+
+
+class FusionDataset(Dataset):
+    def __init__(self, root, is_train=True,with_det=False):
+        self.root = root
+        self.is_train = is_train
+        self.visible_path = os.path.join(self.root, 'vis')
+        self.infrared_path = os.path.join(self.root, 'ir')
+        if with_det:
+            self.ann_path = os.path.join(self.root, 'ann')
+
+        self.visible_img_list = os.listdir(self.visible_path)
+
+
+        self.vis_transform = Compose([
+            ToTensor()
+            ])
+
+        self.inf_transform = Compose([
+            Grayscale(3),
+            ToTensor()
+            ])
+
+
+    def __getitem__(self, index):
+
+        vis = Image.open(os.path.join(self.visible_path, self.visible_img_list[index])).convert('RGB')
+        inf = Image.open(os.path.join(self.infrared_path, self.visible_img_list[index])).convert('L')
+
+        vis = self.vis_transform(vis)
+        inf = self.inf_transform(inf)
+
+        if self.is_train:
+            inf_mask = torch.rand((inf.shape[1],inf.shape[2]))
+            inf[:,inf_mask<0.05] = 0
+        return vis,inf
+
+    def __len__(self):
+        return len(self.visible_img_list)
+    
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    dataset = FusionDataset(root=r'D:\迅雷下载\image_fusion\M3FD_Detection-001')
+    vis, inf = dataset[0]
+    plt.subplot(121)
+    plt.imshow(vis.permute(1,2,0))
+    plt.subplot(122)    
+    plt.imshow(inf.permute(1,2,0))
+    plt.show()
+    
